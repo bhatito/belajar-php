6c06ad410ad3575b98c57d744dd988ac8a72d979
bhatito <bhatitoputra86@gmail.com>
2023-03-30 19:49:26 +0700

    tugas pertemuan 6 / tugas5

---
+++ tugas6pegawai.php
@@ -0,0 +1,18 @@
+<?php 
+require 'tugas6.php';
+
+$pegawai1 = new Pegawai('01111','Andi','Manager','Islam','Menikah');
+$pegawai2 = new Pegawai('01112','Rendy','Asisten Manager','Islam','Lajang');
+$pegawai3 = new Pegawai('01113','Krisna','Kepala Bagian','Islam','Lajang');
+$pegawai4 = new Pegawai('01114','Fajar','Staff','Islam','Menikah');
+$pegawai5 = new Pegawai('01115','Bagong','OB','Islam','Menikah');
+
+
+$ar_pegawai = [$pegawai1,$pegawai2,$pegawai3,$pegawai4,$pegawai5];
+
+foreach($ar_pegawai as $pegawai){
+    $pegawai->cetak();
+}
+
+
+?>
